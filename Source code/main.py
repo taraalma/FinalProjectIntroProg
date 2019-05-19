54287cca87165c2799ccc1ea7a7b1fea71e994c0
taraalma <19585057+taraalma@users.noreply.github.com>
2019-05-19 14:47:55 -0600

    -Added comments to this file.

--- Source code/main.py
+++ Source code/main.py
@@ -1,27 +1,28 @@
 # Final project. Tkinter application.
 
+# import modules
 import time
 from tkinter import *
 from random import *
 
-# gradient for temperature
+# colors for each gradient.
 arrayColores = ['#E50000','#DD0008','#D60010','#CE0018','#C70020','#C00029','#B80031','#B10039','#A90041','#A2004A','#9B0052','#93005A','#8C0062','#84006A','#7D0073','#76007B','#6E0083','#67008B','#600094','#58009C','#5100A4','#4900AC','#4200B4','#3B00BD','#3300C5','#2C00CD','#2400D5','#1D00DE','#1600E6','#0E00EE','#0700F6','#0000FF']
 
+# array for each color of the gradient
 gradientList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
 
 def createAWindow():
 	global arrayColores
 	global gradientList
 	
+	# this creates the main window.
 	window = Tk()
 	window.geometry('1000x600+10+10')
 	canvas = Canvas(window, width=900, height=500)
 	canvas.pack()
 	window.title('generic tkinter window')
 	
-	# This draws the thermometer 
-	
+	# This draws the thermometer. the range indicates how many gradients will be drawn.
 	for x in range (0,32):
 		# modify only the xStartval to indicate where it shoul start
 		xStartval = 30
@@ -33,24 +34,30 @@ def createAWindow():
 		xFinVal = xStartval + 70
 		yFinVal = 350 - ( rectangleHeight * (1 + x) )
 		colorito = str(arrayColores[31 - x])
-		
+		# this initializes each gradient
 		gradientList[x] = canvas.create_rectangle(xStartval, yStartval, xFinVal, yFinVal, width=5 , fill=colorito,outline=colorito)
-		
-
+	
 	createButton(150,120)
 	createUglyText(200,200)
 	window.mainloop()
-	
 
+# this creates a button 
 def createButton(xPos,yPos):
 	mbutton = Button(text='Button Example',state=DISABLED).place(x=xPos,y=yPos)
 
+# this creates an ugly string with text
 def createUglyText(xPos,yPos):
 	textToInput = 'Text Example'
 	mLable = Label(text=textToInput,font=('bold'),fg='#00d7f3',bg='#ffffff').place(x=xPos,y=yPos)
 
+ThreadGUI = threading.Thread(target = createAWindow)
 
-createAWindow()
+
+
+
+
+# this initializes the main window and the whole GUI.
+
 
 
 #time.sleep(60)
