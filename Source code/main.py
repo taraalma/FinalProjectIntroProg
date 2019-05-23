b81873c9095d2df43f68a8513163fcbed4f8056d
taraalma <19585057+taraalma@users.noreply.github.com>
2019-05-23 00:59:34 -0600

    -Modify gradient initialization.

--- Source code/main.py
+++ Source code/main.py
@@ -1,67 +1,121 @@
-# Final project. Tkinter application.
-
-# import modules
-import time
-import threading
 from tkinter import *
-from random import *
+import random
+import time
 from thermostat import *
 
-def createAWindow():
-	global arrayColores
-	global gradientList
-	global numberOfGradients
-	
-	# this creates the main window.
-	window = Tk()
-	window.geometry('1000x600+10+10')
-	canvas = Canvas(window, width=900, height=500)
-	
-	createButton(150,120)
-	
-	labelTimer = uglyLabel(window)
-	
-	canvas.pack()
-	window.title('generic tkinter window')
-	
-	# This draws the thermometer. the range indicates how many gradients will be drawn.
-	for x in range (0,numberOfGradients):
-		# modify only the xStartval to indicate where it shoul start
-		xStartval = 30
-		# modify the rectangleHeight value to indicate the height of the gradient
-		rectangleHeight = 4
-		yStartval = 350 - ( rectangleHeight * x )
-		# modify the rectangleWidth value to indicate the width of the rectangle 
-		rectangleWidth = 70
-		xFinVal = xStartval + 70
-		yFinVal = 350 - ( rectangleHeight * (1 + x) )
-		colorito = str(arrayColores[31 - x])
-		# this initializes each gradient
-		gradientList[x] = canvas.create_rectangle(xStartval, yStartval, xFinVal, yFinVal, width=5 , fill=colorito,outline=colorito)
-	window.mainloop()
+tk = Tk()
+tk.title("Monitor")
+tk.resizable(0,0)
+tk.wm_attributes("-topmost",1)
+canvas = Canvas(tk, width = 500, height = 500, bd = 0, highlightthickness = 0)
+canvas.pack()
+tk.update()
 
-# this creates a button 
-def createButton(xPos,yPos):
-	mbutton = Button(text='Button Example',state=DISABLED).place(x=xPos,y=yPos)
-
-# this creates an ugly string with text
-class uglyLabel:
-	def __init__(self,parent):
-		self.seconds = 0
-		self.label = Label(parent, text="0 s", font="Arial 30", width=10,fg='#00d7f3',bg='#ffffff' )
-		#self.label = Label(parent, text="0 s",font=('bold'),fg='#00d7f3',bg='#ffffff').place(x=xPos,y=yPos)
-		self.label.pack()
-		self.label.after(1000, self.refreshLabel)
+class Gradient:
+	
+	def __init__(self, canvas, xStartval, yStartval, xFinVal, yFinVal, x):
+		global arrayColores
 		
-	def refreshLabel(self):
-		self.seconds += 1
-		self.label.configure(text="%i s" % self.seconds)
-		self.label.after(1000, self.refreshLabel)
+		colorito = str(arrayColores[31 - x])
+		self.canvas = canvas
+		self.id = canvas.create_rectangle(xStartval, yStartval, xFinVal, yFinVal, width=5 , fill=colorito,outline=colorito)
+	def draw(self):
+		pass
 
-Thread_GUI = threading.Thread(target = createAWindow)
+class Paddle:
+	def __init__(self,canvas,color):
+		self.canvas = canvas
+		self.id = canvas.create_rectangle(0,0,100,10,fill=color)
+		self.canvas.move(self.id,200,300)
+		self.x = 0
+		self.canvas_width = self.canvas.winfo_width()
+		self.canvas.bind_all('<KeyPress-Left>',self.turn_left)
+		self.canvas.bind_all('<KeyPress-Right>',self.turn_right)
+	def draw(self):
+		self.canvas.move(self.id,self.x,0)
+		pos = self.canvas.coords(self.id)
+		if pos[0] <= 0:
+			self.x = 0
+		if pos[2] >= self.canvas_width:
+			self.x = 0
+	def turn_left(self,evt):
+		self.x = -2
+	def turn_right(self,evt):
+		self.x = 2
+		
 
-Thread_GUI.start()
+xWidth = 40
 
-# this initializes the main window and the whole GUI.
+Gradient0 = Gradient (canvas,0,0,xWidth,4,31)
+Gradient1 = Gradient (canvas,0,4,xWidth,8,30)
+Gradient2 = Gradient (canvas,0,8,xWidth,12,29)
+Gradient3 = Gradient (canvas,0,12,xWidth,16,28)
+Gradient4 = Gradient (canvas,0,16,xWidth,20,27)
+Gradient5 = Gradient (canvas,0,20,xWidth,24,26)
+Gradient6 = Gradient (canvas,0,24,xWidth,28,25)
+Gradient7 = Gradient (canvas,0,28,xWidth,32,24)
+Gradient8 = Gradient (canvas,0,32,xWidth,36,23)
+Gradient9 = Gradient (canvas,0,36,xWidth,40,22)
+Gradient10 = Gradient (canvas,0,40,xWidth,44,21)
+Gradient11 = Gradient (canvas,0,44,xWidth,48,20)
+Gradient12 = Gradient (canvas,0,48,xWidth,52,19)
+Gradient13 = Gradient (canvas,0,52,xWidth,56,18)
+Gradient14 = Gradient (canvas,0,56,xWidth,60,17)
+Gradient15 = Gradient (canvas,0,60,xWidth,64,16)
+Gradient16 = Gradient (canvas,0,64,xWidth,68,15)
+Gradient17 = Gradient (canvas,0,68,xWidth,72,14)
+Gradient18 = Gradient (canvas,0,72,xWidth,76,13)
+Gradient19 = Gradient (canvas,0,76,xWidth,80,12)
+Gradient20 = Gradient (canvas,0,80,xWidth,84,11)
+Gradient21 = Gradient (canvas,0,84,xWidth,88,10)
+Gradient22 = Gradient (canvas,0,88,xWidth,92,9)
+Gradient23 = Gradient (canvas,0,92,xWidth,96,8)
+Gradient24 = Gradient (canvas,0,96,xWidth,100,7)
+Gradient25 = Gradient (canvas,0,100,xWidth,104,6)
+Gradient26 = Gradient (canvas,0,104,xWidth,108,5)
+Gradient27 = Gradient (canvas,0,108,xWidth,112,4)
+Gradient28 = Gradient (canvas,0,112,xWidth,116,3)
+Gradient29 = Gradient (canvas,0,116,xWidth,120,2)
+Gradient30 = Gradient (canvas,0,120,xWidth,124,1)
+Gradient31 = Gradient (canvas,0,124,xWidth,128,0)
 
-#time.sleep(60)
+
+while 1:
+	Gradient0.draw()
+	Gradient1.draw()
+	Gradient2.draw()
+	Gradient3.draw()
+	Gradient4.draw()
+	Gradient5.draw()
+	Gradient6.draw()
+	Gradient7.draw()
+	Gradient8.draw()
+	Gradient9.draw()
+	Gradient10.draw()
+	Gradient11.draw()
+	Gradient12.draw()
+	Gradient13.draw()
+	Gradient14.draw()
+	Gradient15.draw()
+	Gradient16.draw()
+	Gradient17.draw()
+	Gradient18.draw()
+	Gradient19.draw()
+	Gradient20.draw()
+	Gradient21.draw()
+	Gradient22.draw()
+	Gradient23.draw()
+	Gradient24.draw()
+	Gradient25.draw()
+	Gradient26.draw()
+	Gradient27.draw()
+	Gradient28.draw()
+	Gradient29.draw()
+	Gradient30.draw()
+	Gradient31.draw()
+
+	
+	tk.update_idletasks()
+	tk.update()
+	time.sleep(0.01)
+			
