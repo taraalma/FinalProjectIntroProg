1d07328763f141d49b487015f3421be4268f7b29
taraalma <19585057+taraalma@users.noreply.github.com>
2019-05-24 11:30:45 -0600

    -Gradients can now be hidden

--- Source code/main.py
+++ Source code/main.py
@@ -37,8 +37,13 @@ class Gradient:
 		else:
 			self.id = canvas.create_rectangle(self.xStartingValue, self.yStartingValue, self.xFinalValue, self.yFinalValue, width=5 , fill='#FFFFFF',outline='#FFFFFF')
 
-xWidth = 40
 
+
+
+# Initialize gradients
+# the variable xWidth is used to determine the width of each gradient.
+
+xWidth = 40
 Gradient0 = Gradient (canvas,0,0,xWidth,4,31)
 Gradient1 = Gradient (canvas,0,4,xWidth,8,30)
 Gradient2 = Gradient (canvas,0,8,xWidth,12,29)
@@ -75,17 +80,150 @@ Gradient31 = Gradient (canvas,0,124,xWidth,128,0)
 
 
 while 1:
-	global oddNon
-	
-	if (oddNon == 0):
-		tk.update_idletasks()
-		tk.update()
-		time.sleep(1)
-		oddNon = 1
 	if ( oddNon == 1):
+		Gradient0.IsObjectHidden = 1
+		Gradient1.IsObjectHidden = 1
+		Gradient2.IsObjectHidden = 1
+		Gradient3.IsObjectHidden = 1
+		Gradient4.IsObjectHidden = 1
+		Gradient5.IsObjectHidden = 1
+		Gradient6.IsObjectHidden = 1
+		Gradient7.IsObjectHidden = 1
+		Gradient8.IsObjectHidden = 1
+		Gradient9.IsObjectHidden = 1
+		Gradient10.IsObjectHidden = 1
+		Gradient11.IsObjectHidden = 1
+		Gradient12.IsObjectHidden = 1
+		Gradient13.IsObjectHidden = 1
+		Gradient14.IsObjectHidden = 1
+		Gradient15.IsObjectHidden = 1
+		Gradient16.IsObjectHidden = 1
+		Gradient17.IsObjectHidden = 1
+		Gradient18.IsObjectHidden = 1
+		Gradient19.IsObjectHidden = 1
+		Gradient20.IsObjectHidden = 1
+		Gradient21.IsObjectHidden = 1
+		Gradient22.IsObjectHidden = 1
+		Gradient23.IsObjectHidden = 1
+		Gradient24.IsObjectHidden = 1
+		Gradient25.IsObjectHidden = 1
+		Gradient26.IsObjectHidden = 1
+		Gradient27.IsObjectHidden = 1
+		Gradient28.IsObjectHidden = 1
+		Gradient29.IsObjectHidden = 1
+		Gradient30.IsObjectHidden = 1
+		Gradient31.IsObjectHidden = 1
+		
+		Gradient0.draw(canvas)
+		Gradient1.draw(canvas)
+		Gradient2.draw(canvas)
+		Gradient3.draw(canvas)
+		Gradient4.draw(canvas)
+		Gradient5.draw(canvas)
+		Gradient6.draw(canvas)
+		Gradient7.draw(canvas)
+		Gradient8.draw(canvas)
+		Gradient9.draw(canvas)
+		Gradient10.draw(canvas)
+		Gradient11.draw(canvas)
+		Gradient12.draw(canvas)
+		Gradient13.draw(canvas)
+		Gradient14.draw(canvas)
+		Gradient15.draw(canvas)
+		Gradient16.draw(canvas)
+		Gradient17.draw(canvas)
+		Gradient18.draw(canvas)
+		Gradient19.draw(canvas)
+		Gradient20.draw(canvas)
+		Gradient21.draw(canvas)
+		Gradient22.draw(canvas)
+		Gradient23.draw(canvas)
+		Gradient24.draw(canvas)
+		Gradient25.draw(canvas)
+		Gradient26.draw(canvas)
+		Gradient27.draw(canvas)
+		Gradient28.draw(canvas)
+		Gradient29.draw(canvas)
+		Gradient30.draw(canvas)
+		Gradient31.draw(canvas)
+
+		
 		tk.update_idletasks()
 		tk.update()
-		time.sleep(1)
+		time.sleep(0.5)
 		oddNon = 0
 
-
+	elif(oddNon == 0):
+	
+		Gradient0.IsObjectHidden = 0
+		Gradient1.IsObjectHidden = 0
+		Gradient2.IsObjectHidden = 0
+		Gradient3.IsObjectHidden = 0
+		Gradient4.IsObjectHidden = 0
+		Gradient5.IsObjectHidden = 0
+		Gradient6.IsObjectHidden = 0
+		Gradient7.IsObjectHidden = 0
+		Gradient8.IsObjectHidden = 0
+		Gradient9.IsObjectHidden = 0
+		Gradient10.IsObjectHidden = 0
+		Gradient11.IsObjectHidden = 0
+		Gradient12.IsObjectHidden = 0
+		Gradient13.IsObjectHidden = 0
+		Gradient14.IsObjectHidden = 0
+		Gradient15.IsObjectHidden = 0
+		Gradient16.IsObjectHidden = 0
+		Gradient17.IsObjectHidden = 0
+		Gradient18.IsObjectHidden = 0
+		Gradient19.IsObjectHidden = 0
+		Gradient20.IsObjectHidden = 0
+		Gradient21.IsObjectHidden = 0
+		Gradient22.IsObjectHidden = 0
+		Gradient23.IsObjectHidden = 0
+		Gradient24.IsObjectHidden = 0
+		Gradient25.IsObjectHidden = 0
+		Gradient26.IsObjectHidden = 0
+		Gradient27.IsObjectHidden = 0
+		Gradient28.IsObjectHidden = 0
+		Gradient29.IsObjectHidden = 0
+		Gradient30.IsObjectHidden = 0
+		Gradient31.IsObjectHidden = 0
+		
+		Gradient0.draw(canvas)
+		Gradient1.draw(canvas)
+		Gradient2.draw(canvas)
+		Gradient3.draw(canvas)
+		Gradient4.draw(canvas)
+		Gradient5.draw(canvas)
+		Gradient6.draw(canvas)
+		Gradient7.draw(canvas)
+		Gradient8.draw(canvas)
+		Gradient9.draw(canvas)
+		Gradient10.draw(canvas)
+		Gradient11.draw(canvas)
+		Gradient12.draw(canvas)
+		Gradient13.draw(canvas)
+		Gradient14.draw(canvas)
+		Gradient15.draw(canvas)
+		Gradient16.draw(canvas)
+		Gradient17.draw(canvas)
+		Gradient18.draw(canvas)
+		Gradient19.draw(canvas)
+		Gradient20.draw(canvas)
+		Gradient21.draw(canvas)
+		Gradient22.draw(canvas)
+		Gradient23.draw(canvas)
+		Gradient24.draw(canvas)
+		Gradient25.draw(canvas)
+		Gradient26.draw(canvas)
+		Gradient27.draw(canvas)
+		Gradient28.draw(canvas)
+		Gradient29.draw(canvas)
+		Gradient30.draw(canvas)
+		Gradient31.draw(canvas)
+		
+		tk.update_idletasks()
+		tk.update()
+		time.sleep(0.5)
+		oddNon = 1
+	else:
+		pass
