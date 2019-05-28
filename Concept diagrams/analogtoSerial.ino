0f94ab7fa975b7ca43857d89177ffebb80db79dd
taraalma <19585057+taraalma@users.noreply.github.com>
2019-05-27 22:36:18 -0600

    ino for arduino

---
+++ Concept diagrams/analogtoSerial.ino
@@ -0,0 +1,83 @@
+/*
+  Analog input, analog output, serial output
+
+  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
+  the result to set the pulse width modulation (PWM) of an output pin.
+  Also prints the results to the Serial Monitor.
+
+  The circuit:
+  - potentiometer connected to analog pin 0.
+    Center pin of the potentiometer goes to the analog pin.
+    side pins of the potentiometer go to +5V and ground
+  - LED connected from digital pin 9 to ground
+
+  created 29 Dec. 2008
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
+*/
+
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 13; // Analog output pin that the LED is attached to
+
+const int numReadings = 1;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(57600);
+  analogReadResolution(12);
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+}
+}
+
+void loop() {
+  
+  // subtract the last reading:
+  total = total - readings[readIndex];
+ 
+  
+  
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+  // map it to the range of the analog out:
+  outputValue = map(sensorValue, 0, 4095, 0, 140);
+  outputValue = outputValue - 85;
+  if ( outputValue < 0)
+  {outputValue = 0;}
+  if(outputValue > 31 )
+  {outputValue = 31; }
+  readings[readIndex] = outputValue;
+
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  // send it to the computer as ASCII digits
+  
+  Serial.println(average);
+  //Serial.println();
+  delay(2000);        // delay in between reads for stability
+
+}
